45467ff0d78bb9313482644dc768766fed68eb5f
lemartin19 <lmartin411@gmail.com>
2021-01-26 15:45:17 +0000

    fix some style, add notes to calculator

--- hw02/calc.js
+++ hw02/calc.js
@@ -1,8 +1,13 @@
 (() => {
+    // wrapped all of these in a closure to prevent "globals"
+    // cannot move these out into different files for the same
+    // reason, which is why the organization is only separated
+    // by comments
+
     // HELPERS
     const doOperation = ({ currentVal, prevVal, operation }) => {
-        // if you press two operation keys in a row, don't throw an error
-        // (required to press equal sign and then use the result)
+        // if you press two operation keys in a row, don't throw an
+        // error (required for +/= combination)
         if (isNaN(currentVal)) return prevVal;
     
         const prev = Number(prevVal);
@@ -10,6 +15,10 @@
         return `${operation(prev, curr)}`;
     };
     
+    // by using the default JavaScript operations, we are relying on
+    // the default behavior of the language, meaning that the numbers
+    // will start being rounded at 16 digits and will go to scientific
+    // notation at about 20 digits
     const add = (a, b) => a + b;
     
     const subtract = (a, b) => a - b;
@@ -38,21 +47,24 @@
             case '*': return multiply;
             case '/': return divide;
             default:
-                throw new Error(`unexpected operation - received: ${val}`);
+                throw new Error(`unexpected operation: ${val}`);
         }
     };
 
     const setResultText = ({ currentVal, prevVal }) => {
         const result = document.getElementById('result');
-        result.innerText = isNaN(currentVal) ? Number(prevVal) : Number(currentVal);
+        result.innerText = isNaN(currentVal)
+            ? Number(prevVal)
+            : Number(currentVal);
     };
 
     // STATE
     const state = { currentVal: '0', prevVal: '0', operation: add };
     const updateState = (val) => {
+        const current = state.currentVal;
         switch (val) {
             case '.':
-                state.currentVal = handleDecimal(state.currentVal, val);
+                state.currentVal = handleDecimal(current, val);
                 break;
             case '0':
             case '1':
@@ -64,7 +76,7 @@
             case '7':
             case '8':
             case '9':
-                state.currentVal = handleNumber(state.currentVal, val);
+                state.currentVal = handleNumber(current, val);
                 break;
             case '+':
             case '-':
